45e381b2dd60941bf9cd176f4646178b4f56d505
Aidan Purchase <aidantpurchase@gmail.com>
2020-06-21 21:11:51 +0000

    Split --play into --create and --join

--- mafiabot.py
+++ mafiabot.py
@@ -5,59 +5,72 @@ class MafiaBot(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.games = {}
-        self.open_games = {}
+        self.open_games = set([])
 
     @commands.Cog.listener()
     async def on_ready(self):
         print("Mafia Bot ready")
 
     @commands.command(pass_context=True)
-    async def play(self, ctx):
+    async def create(self, ctx):
         if not isinstance(ctx.channel, discord.channel.DMChannel):
             game = ctx.message.content.split(" ")[1]
-            #TODO make a create command so I can avoid this error
-            if game in self.games and self.open_games[ctx.guild]:
-                self.games[game].append(self.message.author)
-            elif self.open_games[ctx.guild]:
-                self.games[game] = [ctx.message.author]
+            if game in self.games:
+                await ctx.send("{} has already been created! Type '--join {}' to join the game.".format(game, ctx.message.author, game))
             else:
-                await ctx.send("{} is unable to join the game".format(ctx.message.author))
+                self.games[game] = [ctx.message.author]
+                self.open_games.add(game)
+                await ctx.send("{} has been created by {}! Type '--join {}' to join the game.".format(game, ctx.message.author, game))
         else:
-            await ctx.send("Please join a server to use this command")
+            await ctx.send("Please join a server to use this command.")
+
+    @commands.command(pass_context=True)
+    async def join(self, ctx):
+        if not isinstance(ctx.channel, discord.channel.DMChannel):
+            game = ctx.message.content.split(" ")[1]
+            if game in self.games and self.open_games:
+                self.games[game].append(self.message.author)
+            else:
+                await ctx.send("{} is unable to join the game. Try again later.".format(ctx.message.author))
+        else:
+            await ctx.send("Please join a server to use this command.")
 
     @commands.command(pass_context=True)
     async def start(self, ctx):
         if not isinstance(ctx.channel, discord.channel.DMChannel):
             game = ctx.message.content.split(" ")[1]
             if game in self.games:
-                if ctx.message.author == self.games[game][ctx.message.author]:
-                    self.open_games[game] = False
+                #TODO Make sure dead games (ie, creator hasn't ended it) are removed
+                if ctx.message.author == self.games[game][0]:
+                    self.open_games.remove(game)
+                    await ctx.send("{} has been started!!".format(game))
                     #TODO get the game actually started
                 else:
-                    await ctx.send("{} doesn't have permission to start {}".format(ctx.message.author, game))
+                    await ctx.send("{} doesn't have permission to start {}!".format(ctx.message.author, game))
             else:
-                await ctx.send("{} doesn't exist".format(game))
+                await ctx.send("{} doesn't exist!".format(game))
         else:
-            await ctx.send("Please join a server to use this command")
+            await ctx.send("Please join a server to use this command.")
 
     @commands.command(pass_context=True)
     async def kill(self, ctx):
         if isinstance(ctx.channel, discord.channel.DMChannel):
             print("{} killed nothing".format(ctx.message.author))
         else:
-            print("{} made a big whoopsie".format(ctx.message.author))
+            print("{} made a big whoopsie!".format(ctx.message.author))
 
-    @commands.command(pass_context=True)
-    async def join(self, ctx):
-        try:
-            channel = ctx.message.author.voice.channel
-            await channel.connect()
-        except Exception as error:
-            print("mafiabot couldn't join vc. [{}]".format(error))
+#TODO remove these once you've sorted out vc aspect of bot
+#    @commands.command(pass_context=True)
+#    async def join(self, ctx):
+#        try:
+#            channel = ctx.message.author.voice.channel
+#            await channel.connect()
+#        except Exception as error:
+#            print("mafiabot couldn't join vc. [{}]".format(error))
 
-    @commands.command(pass_context=True)
-    async def leave(self, ctx):
-        await ctx.voice_client.disconnect()
+#    @commands.command(pass_context=True)
+#    async def leave(self, ctx):
+#        await ctx.voice_client.disconnect()
 
     @commands.command(pass_context=True)
     async def help(self, ctx):
@@ -68,8 +81,9 @@ class MafiaBot(commands.Cog):
         )
 
         embed.set_author(name="Help")
-        embed.add_field(name="--play [game_name]", value="Instantiate or join the specified game", inline=False)
-        embed.add_field(name="--start [game_name]", value="Starts the specified game (only if you instantiated it", inline=False)
+        embed.add_field(name="--create [game_name]", value="Create the specified game", inline=False)
+        embed.add_field(name="--join [game_name]", value="Join the specified game", inline=False)
+        embed.add_field(name="--start [game_name]", value="Starts the specified game (only if you created it)", inline=False)
         await author.send(embed=embed)
 
     @commands.command(pass_context=True)
