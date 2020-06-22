3b4dac6571f4b59ee4b9e89f3307226da5dbb431
Aidan Purchase <aidantpurchase@gmail.com>
2020-06-22 11:07:20 +0000

    Added --delete command

--- mafiabot.py
+++ mafiabot.py
@@ -57,6 +57,25 @@ class MafiaBot(commands.Cog):
             await ctx.send("Please join a server voice chat to use this command.")
 
     @commands.command(pass_context=True)
+    async def delete(self, ctx):
+        if not isinstance(ctx.channel, discord.channel.DMChannel):
+            game = ctx.message.content.split(" ")[1]
+            if game in self.games:
+                if ctx.message.author == self.games[game][0]:
+                    self.games.pop(game, None)
+                    try:
+                        self.open_games.remove(game) # A game can be ended whether it is open or not
+                    except:
+                        pass 
+                    await ctx.send("{} was been deleted!".format(game))
+                else:
+                    await ctx.send("{} doesn't have permission to delete {}".format(ctx.message.author, game))
+            else:
+                await ctx.send("{} doesn't exist!".format(game))
+        else:
+            await ctx.send("Please join a server to use this command.")
+
+    @commands.command(pass_context=True)
     async def kill(self, ctx):
         if isinstance(ctx.channel, discord.channel.DMChannel):
             print("{} killed nothing".format(ctx.message.author))
@@ -64,14 +83,6 @@ class MafiaBot(commands.Cog):
             print("{} made a big whoopsie!".format(ctx.message.author))
 
 #TODO remove these once you've sorted out vc aspect of bot
-#    @commands.command(pass_context=True)
-#    async def join(self, ctx):
-#        try:
-#            channel = ctx.message.author.voice.channel
-#            await channel.connect()
-#        except Exception as error:
-#            print("mafiabot couldn't join vc. [{}]".format(error))
-
 #    @commands.command(pass_context=True)
 #    async def leave(self, ctx):
 #        await ctx.voice_client.disconnect()
@@ -88,6 +99,7 @@ class MafiaBot(commands.Cog):
         embed.add_field(name="--create [game_name]", value="Create the specified game", inline=False)
         embed.add_field(name="--join [game_name]", value="Join the specified game", inline=False)
         embed.add_field(name="--start [game_name]", value="Starts the specified game (only if you created it)", inline=False)
+        embed.add_field(name="--delete [game_name]", value="Deletes the specified game (only if you created it)", inline=False)
         await author.send(embed=embed)
 
     @commands.command(pass_context=True)
