1f5cc4e72a5222b3557b2a5cac9f329a7db22f79
Aidan Purchase <aidantpurchase@gmail.com>
2020-06-25 09:50:40 +0000

    Updated with database: get_players

--- game.py
+++ game.py
@@ -1,21 +1,65 @@
 import random
+from gamelist import GameList
 
 town_roles = ["inspector","doctor","drunk","bodyguard"]
 mafia_roles = ["boss","disabler","stalker"]
 
 class Game:
-    def __init__(self, players, num_mafia, num_roles):
-        self.players = random.shuffle(players)
-        self.setup = {}
+    instance = None
+
+    def __init__(self):
+        GameList.instance = GameList()
+        
+    def new_game(self, name, players, num_mafia, num_roles):
+        author = players[0]
+        gameID = GameList.instance.create_game(name, author)
+
+        shuffled_players = random.shuffle(players)
+        num_townies = len(players) - num_mafia
 
         for i in range(min(num_mafia, 3)):
             role = mafia_roles[i]
-            self.setup[role] = self.players[i]
+            player = shuffled_players[i]
+            GameList.instance.create_attendance(player, gameID, role)
         for i in range(min(num_roles, 4)):
             role = town_roles[i]
-            self.setup[role] = self.players[num_mafia+i]
+            player = shuffled_players[num_mafia+i]
+            GameList.instance.create_attendance(player, gameID, role)
+        for player in shuffled_players[num_mafia+num_roles:]:
+            GAmeList.instance.craete_attendance(player, gameID, "townie")
 
-        self.setup["townie"] = self.players[num_mafia+num_roles:]
+        return gameID
+
+    def get_players(self, gameID, role):
+        roles = GameList.instance.get_roles(gameID)
+        players = []
+        
+        for role_pair in roles:
+            temp_role, member = role_pair
+            if temp_role == role:
+                players.append(member)
+
+        if len(players) == 1:
+            return players[0]
+        else:
+            return players
+
+    def remove_player(self, player):
+        for _player in self.setup["townie"]:
+            if player == _player:
+                self.num_townies -= 1
+
+        for role, _player in self.setup.items():
+            if player == _player:
+                if role in town_roles:
+                    self.num_townies -= 1
+                else:
+                    self.num_mafia -= 1
 
     def has_finished(self):
-        pass
+        if self.num_mafia <= 0:
+            return "Town"
+        elif self.num_townies <= 0:
+            return "Mafia"
+        else:
+            return None
