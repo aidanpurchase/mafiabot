f2e6decea52600c0ce382f4f6331be09dae30110
Aidan Purchase <aidantpurchase@gmail.com>
2020-07-06 16:28:49 +0000

    Removed superfluous --kill command

--- mafiabot.py
+++ mafiabot.py
@@ -152,29 +152,6 @@ class MafiaBot(commands.Cog):
             return None
 
     @commands.command(pass_context=True)
-    async def kill(self, ctx):
-        if isinstance(ctx.channel, discord.channel.DMChannel):
-            if ctx.author.id is in GameList.instance.get_playing_IDs():
-                if GameList.instance.get_role(ctx.author.id) == "Boss":
-                    game_name = GameList.instance.get_game(ctx.author)
-                    try:
-                        target = ctx.message.mentions.users.first()
-                    except:
-                        await ctx.send("Target does not exist! Type --kill @[user_name] (not nickname).")
-                    
-                    if target.id is in GameList.instance.get_alive_IDs(game_name):
-                        GameList.instance.kill_player(game_name, target)
-                        await ctx.send("You successfully targetted {}.".format(target.name))
-                    else:
-                        await ctx.send("Target is either already dead or not playing! Type --kill @[username] (not nickname).")
-                else:
-                    await ctx.send("You must be a Boss to use this command! Type --roles to find your role!")
-            else:
-                await ctx.send("you must be in an active game to use this command.")
-        else:
-            await ctx.send("You must be in a DM with me to use this command!")
-
-    @commands.command(pass_context=True)
     async def help(self, ctx):
         author = ctx.author
 
