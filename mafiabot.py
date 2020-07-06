e39af924a48125bd1b46f245d133719bb1fc5e6f
Aidan Purchase <aidantpurchase@gmail.com>
2020-07-06 15:38:48 +0000

    Added --kill command only for Boss

--- mafiabot.py
+++ mafiabot.py
@@ -154,9 +154,25 @@ class MafiaBot(commands.Cog):
     @commands.command(pass_context=True)
     async def kill(self, ctx):
         if isinstance(ctx.channel, discord.channel.DMChannel):
-            print("{} killed nothing".format(ctx.message.author))
+            if ctx.message.author.id is in GameList.instance.get_playing_IDs():
+                if GameList.instance.get_role(ctx.message.author.id) == "Boss":
+                    game_name = GameList.instance.get_game(ctx.message.author)
+                    try:
+                        target = ctx.message.mentions.users.first()
+                    except:
+                        await ctx.send("Target does not exist! Type --kill @[user_name] (not nickname).")
+                    
+                    if target.id is in GameList.instance.get_alive_IDs(game_name):
+                        GameList.instance.kill_player(game_name, target)
+                        await ctx.send("You successfully targetted {}.".format(target.name))
+                    else:
+                        await ctx.send("Target is either already dead or not playing! Type --kill @[username] (not nickname).")
+                else:
+                    await ctx.send("You must be a Boss to use this command! Type --roles to find your role!")
+            else:
+                await ctx.send("you must be in an active game to use this command.")
         else:
-            print("{} made a big whoopsie!".format(ctx.message.author))
+            await ctx.send("You must be in a DM with me to use this command!")
 
     @commands.command(pass_context=True)
     async def help(self, ctx):
